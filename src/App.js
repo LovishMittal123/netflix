7f4fc9c3457f3037ed4dab78f220b4ec781aee32
lovish <lovish0429.be20@chitkara.edu.in>
2024-06-26 16:11:14 +0530

    Your commit message

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+const App = () => {
+  return (
+    <div className='text-3xl font-bold text-green-600'>
+      Hello
+    </div>
+  )
+}
+
+export default App
